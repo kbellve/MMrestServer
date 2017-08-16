12d82ac5ccb5597075e7c51be21138cf9914148a
kbellve <kdb@oxygen.umassmed.edu>
2017-08-16 14:16:29 -0400

    fixed bug not displaying overview.html

--- src/edu/umassmed/big/mmremote/handlers/PageHandler.java
+++ src/edu/umassmed/big/mmremote/handlers/PageHandler.java
@@ -1,39 +1,35 @@
 package edu.umassmed.big.mmremote.handlers;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import edu.umassmed.big.mmremote.mmKNIME;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-
 /**
- *  Handle loading a page. In essence, this is an abstract wrapper around a file
- *  reader for a single HTML page. Extending classes should implement the
- *  getFileStream method, and optionally load additional data required for 
- *  displaying the file.
- * 
- *  @author Matthijs
+ * Handle loading a page. In essence, this is an abstract wrapper around a file
+ * reader for a single HTML page. Extending classes should implement the
+ * getFileStream method, and optionally load additional data required for
+ * displaying the file.
+ *
+ * @author Matthijs
  */
-abstract public class PageHandler implements HttpHandler  {
+abstract public class PageHandler implements HttpHandler {
 
-    
-    @Override
-    public void handle(HttpExchange exchange) throws IOException {
-    	mmKNIME.core.logMessage("µmKNIME: Handling overview request.");        
-        
-        java.util.Scanner scanner   = new java.util.Scanner(getFileStream()).useDelimiter("\\A");
-        String response             = scanner.hasNext() ? scanner.next() : "";
-        exchange.sendResponseHeaders(200, response.length());
-        OutputStream os     = exchange.getResponseBody();
-        os.write(response.getBytes());
-        os.flush();
-        os.close();
-    }
-    
-    
-    abstract protected InputStream getFileStream ();
+	abstract protected InputStream getFileStream();
+
+	@Override
+	public void handle(final HttpExchange exchange) throws IOException {
+		mmKNIME.core.logMessage("µmKNIME: Handling overview request.");
+
+		final java.util.Scanner scanner = new java.util.Scanner(this.getFileStream()).useDelimiter("\\A");
+		final String response = scanner.hasNext() ? scanner.next() : "";
+		exchange.sendResponseHeaders(200, response.length());
+		final OutputStream os = exchange.getResponseBody();
+		os.write(response.getBytes());
+		os.close();
+	}
 }
